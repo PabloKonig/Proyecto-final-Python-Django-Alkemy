f508bc828b929b92581852c5c1fd675869f224b7
PabloKonig <pablokonig@yahoo.com>
2024-04-30 17:37:12 -0300

    Se agregaron los modelos Proveedor y Producto y se realizó la migración.

--- src/compra/models.py
+++ src/compra/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Proveedor(models.Model):
+    """Clase que representa a los proveedores de los productos """
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    dni = models.BigIntegerField()        # Para no preocuparnos si se escala con los números
+
+
+class Producto(models.Model):
+    """ Clase que representa a los productos """
+    nombre = models.CharField(max_length=50)
+    precio = models.FloatField()
+    stock_actual = models.IntegerField()
+    proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
