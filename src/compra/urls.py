e0cb2d2d9d4982e52f27ef89c1c80bd21d82e722
PabloKonig <pablokonig@yahoo.com>
2024-05-05 23:59:48 -0300

    Agregado de vistas create

--- src/compra/urls.py
+++ src/compra/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import ProveedoresListView, ProductosListView
+from .views import ProveedoresListView, ProductosListView, ProductoCreateView, ProveedoreCreateView
 
 app_name = 'compra'
 # Se definen las sub-rutas de la app y su conexión con las vistas.
 urlpatterns = [
     path('proveedores/listado/', ProveedoresListView.as_view(), name='proveedores_list'),
-    path('productos/listado/', ProductosListView.as_view(), name='productos_list'),    
+    path('productos/listado/', ProductosListView.as_view(), name='productos_list'),
+    path('productos/crear/', ProductoCreateView.as_view(), name='productos_create'),
+    path('proveedores/crear/', ProveedoreCreateView.as_view(), name='proveedores_create')    
 ]
--- src/compra/views.py
+++ src/compra/views.py
@@ -9,7 +9,19 @@ class ProveedoresListView(generic.ListView):            # Vista basada en clases
     template_name = "proveedores/proveedores_list.html"
     context_object_name = 'lista_de_proveedores'
 
+class ProveedoreCreateView(CreateView):
+    model = Proveedor                                   # Vista basada en clases para crear un proveedor.
+    fields = ['nombre', 'apellido', 'dni']              # Campos que se envían a la plantilla.
+    template_name = "proveedores/proveedores_create.html"
+    success_url = 'http://localhost:8000/compras/proveedores/crear/'
+
 class ProductosListView(generic.ListView):              # Vista basada en clases para listar productos.
     queryset = Producto.objects.all()
     template_name = "productos/productos_list.html"
     context_object_name = 'lista_de_productos'
+
+class ProductoCreateView(CreateView):                   # Vista basada en clases para crear un producto.
+    model = Producto
+    fields = ['nombre', 'precio', 'stock_actual', 'foto', 'proveedor']
+    template_name = "productos/productos_create.html"
+    success_url = 'http://localhost:8000/compras/productos/crear/'
